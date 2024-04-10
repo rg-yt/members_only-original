bcb4058884539074c78b17ce9f64742c94487576
rg-yt <103154533+rg-yt@users.noreply.github.com>
2024-04-09 23:55:44 -0400

    Add new create and index methods to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,25 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create]
   def new
+    @post = Post.new
   end
 
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save!
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def index
+    @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
